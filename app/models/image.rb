f31566ee77b1ca2b5d5c366b27a1f226d2d3d931
Rachel Sakry <rachelsakry@gmail.com>
2012-10-15 11:27:49 -0700

    added validation to image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,12 @@
 class Image < ActiveRecord::Base
-  has_attached_file :photo, :styles => {thumbnail: "100x100>"}
-
   attr_accessible :caption, :photo
 
+  has_attached_file :photo, :styles => {thumbnail: "100x100>"}
 
+  validates_attachment :photo, :presence => true,
+  :content_type => { :content_type => /image/ },
+  :size => { :in => 0..10000.kilobytes }
 
   belongs_to :user
+
 end
